9e1e40bc3f4ef1ede6fec66e1d4f9140dbb39448
david hdez lemus <drhernandezl01@iesalbarregas.es>
2024-12-02 11:57:36 +0100

    update counter
    añadido css en el contador

--- src/components/counter/js/index.js
+++ src/components/counter/js/index.js
@@ -1,42 +1,29 @@
 function handleTickInit(tick) {
-  // uncomment to set labels to different language
-  /*
   var locale = {
-    YEAR_PLURAL: 'Jaren',
-    YEAR_SINGULAR: 'Jaar',
-    MONTH_PLURAL: 'Maanden',
-    MONTH_SINGULAR: 'Maand',
-    WEEK_PLURAL: 'Weken',
-    WEEK_SINGULAR: 'Week',
-    DAY_PLURAL: 'Dagen',
-    DAY_SINGULAR: 'Dag',
-    HOUR_PLURAL: 'Uren',
-    HOUR_SINGULAR: 'Uur',
-    MINUTE_PLURAL: 'Minuten',
-    MINUTE_SINGULAR: 'Minuut',
-    SECOND_PLURAL: 'Seconden',
-    SECOND_SINGULAR: 'Seconde',
-    MILLISECOND_PLURAL: 'Milliseconden',
-    MILLISECOND_SINGULAR: 'Milliseconde'
+    YEAR_PLURAL: 'Años',
+    YEAR_SINGULAR: 'Año',
+    MONTH_PLURAL: 'Meses',
+    MONTH_SINGULAR: 'Mes',
+    WEEK_PLURAL: 'Semanas',
+    WEEK_SINGULAR: 'Semana',
+    DAY_PLURAL: 'Días',
+    DAY_SINGULAR: 'Día',
+    HOUR_PLURAL: 'Horas',
+    HOUR_SINGULAR: 'Hora',
+    MINUTE_PLURAL: 'Minutos',
+    MINUTE_SINGULAR: 'Minuto',
+    SECOND_PLURAL: 'Segundos',
+    SECOND_SINGULAR: 'Segundo',
+    MILLISECOND_PLURAL: 'Milisegundos',
+    MILLISECOND_SINGULAR: 'Milisegundo'
   };
 
   for (var key in locale) {
     if (!locale.hasOwnProperty(key)) { continue; }
     tick.setConstant(key, locale[key]);
   }
-  */
 
-  // format of due date is ISO8601
-  // https://en.wikipedia.org/wiki/ISO_8601
-
-  // '2018-01-31T12:00:00'        to count down to the 31st of January 2018 at 12 o'clock
-  // '2019'                       to count down to 2019
-  // '2018-01-15T10:00:00+01:00'  to count down to the 15th of January 2018 at 10 o'clock in timezone GMT+1
-
-  // set the countdown date to January 5, 2025
   var countdownDate = "2025-02-03T00:00:00+01:00";
-
-  // create the countdown counter
   var counter = Tick.count.down(countdownDate);
 
   counter.onupdate = function (value) {
@@ -44,11 +31,6 @@ function handleTickInit(tick) {
   };
 
   counter.onended = function () {
-    // redirect, uncomment the next line
-    // window.location = 'my-location.html'
-    // hide counter, uncomment the next line
-    // tick.root.style.display = 'none';
-    // show message, uncomment the next line
-    // document.querySelector('.tick-onended-message').style.display = '';
+
   };
 }
